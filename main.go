5c0a8df41cdad05f7ceb8452b529088b266fbb08
Gabe Jensen <thegabejensen@gmail.com>
2024-05-14 07:27:31 -0700

    Actually do help menu

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"path/filepath"
 )
 
+var help = flag.Bool("h", false, "Print help menu")
+var analyze = flag.Bool("a", false, "Analyze repo and gather stats")
+var config = flag.String("c", "", "Specify path to config file. (see https://yearendrecap.com/help#config)")
+var view = flag.Bool("v", false, "View stats in a local presentation")
+var upload = flag.Bool("u", false, "Upload stats to the cloud, to be viewed anywhere")
+
+func init() {
+	flag.Parse()
+}
+
+func printHelp() {
+	fmt.Println("Year End Recap CLI")
+	fmt.Println()
+	flag.PrintDefaults()
+}
+
 func main() {
-	fmt.Println("Hello Year End Recap!")
-	fmt.Println()
-	fmt.Println("Here is a random file path:")
-	fmt.Println()
-  s := filepath.Join("src", "components", "features")
-  fmt.Println(s)
+	if *help {
+		printHelp()
+	} else if *analyze {
+		if *config == "" {
+			fmt.Println("Analyzing with manual prompts...")
+		} else {
+			fmt.Println("Analyzing using config:", *config)
+		}
+	} else if *view {
+		fmt.Println("Viewing stats...")
+	} else if *upload {
+		fmt.Println("Uploading stats to the cloud...")
+	} else {
+		printHelp()
+	}
 }
