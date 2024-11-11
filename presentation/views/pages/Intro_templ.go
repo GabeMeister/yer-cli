bad4ee13ac9f4edbce832fb60349086f75b91f9e
Gabe Jensen <thegabejensen@gmail.com>
2024-11-11 15:22:56 -0800

    Remove hello and added gitattributes

--- presentation/views/pages/Intro_templ.go
+++ presentation/views/pages/Intro_templ.go
@@ -70,15 +70,7 @@ func Intro(recap analyzer.Recap) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><div class=\"py-8\"></div>")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Err = presentation_views_components.NextButton("/hello").Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
