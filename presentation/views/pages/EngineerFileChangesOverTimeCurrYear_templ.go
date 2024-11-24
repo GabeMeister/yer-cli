57bc2a5bdb57b8227dc8a1aa9c7290c73adf4979
Gabe Jensen <thegabejensen@gmail.com>
2024-11-23 16:08:29 -0800

    Removed one more place

--- presentation/views/pages/EngineerFileChangesOverTimeCurrYear_templ.go
+++ presentation/views/pages/EngineerFileChangesOverTimeCurrYear_templ.go
@@ -74,10 +74,6 @@ func EngineerFileChangesOverTimeCurrYear(recap analyzer.Recap) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<script type=\"module\" src=\"/scripts/racing-bar-chart.js\"></script>")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
 		return templ_7745c5c3_Err
 	})
 }
